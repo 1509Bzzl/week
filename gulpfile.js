2bc154263ee747abc0c59a41de99f020c2dbc1a3
1509Bzzl <2226987448@qq.com>
2017-10-09 09:54:38 +0800

    week

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,17 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
+var connect = require('gulp-connect');
 
 gulp.task('server',function () {
     gulp.src('js/*.js')
     .pipe(uglify())
     .pipe(gulp.dest('big.js'))
+})
+gulp.task('connect',function(){
+    gulp.src('dist/main.js')
+    .pipe(connect.connect({
+        port:3030,
+        livereload:true
+    }))
+    
 })
